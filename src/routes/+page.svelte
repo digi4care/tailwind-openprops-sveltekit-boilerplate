ed6137b7c745b0e4ec9f256bcf881f3843892fb6
Chris Engelhard <chris@chrisengelhard.nl>
2025-05-30 09:58:13 +0000

    style: add OpenProps theme configurations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
+<h1 class="text-header text-facebook-logo">Welcome to SvelteKit</h1>
+<p class="text-paragraph">
 	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
 	documentation
 </p>
